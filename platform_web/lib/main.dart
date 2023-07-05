66f9086515814e8ca22e9474a4ba1faca327d9a1
zsefvcx <zsefvcx@list.ru>
2023-07-05 11:59:15 +0300

    lesson16 web 60%

--- platform_web/lib/main.dart
+++ platform_web/lib/main.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart' as http;
+
+///
+///How chrome extensions be enabled when flutter web debugging?
+///1- Go to flutter\bin\cache and remove a file named: flutter_tools.stamp
+///2- Go to flutter\packages\flutter_tools\lib\src\web and open the file chrome.dart.
+///3- Find '--disable-extensions' and comment the line
+///4- Run 'flutter clean' (no 100% sur its needed)
+///
+
+
+
+
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +46,41 @@ class NewWidget extends StatefulWidget {
 
 class _NewWidgetState extends State<NewWidget> {
   final TextEditingController _editController = TextEditingController();
+  String _htmlText = '';
+  String _cors = 'CORS';
+  String _htmlTitle = '';
+  Future<void> loadHtmlPage() async {
+    String? elem;
+    String? body;
+    try {
+      final result = await http.get(Uri.parse(_editController.text),
+      // headers: {
+      //   'Content-Type': 'application/x-www-form-urlencoded',
+      //   'Origin':_editController.text,
+      //   'Access-Control-Allow-Origin': _editController.text
+      // }
+      );
+      if (result.statusCode != 200) {
+        throw('Error request! statusCode:${result
+          .statusCode}');
+      }
+      body = result.body;
+      var doc = parse(result.body);
+      elem = doc.querySelectorAll("head > title:")[0].innerHtml;
+      //var elem2 = doc.querySelectorAll("head > title:")[0];
+      print(result.headers);
+    } catch (e){
+      print(e);
+    }
+    final title = elem??'no title';
+    setState(() {
+      _htmlTitle= title.replaceAll('\n', '').replaceAll('  ', '');
+      _cors = 'CORS';
+      _htmlText = body??'no body';
+    });
+
+
+  }
 
   @override
   void initState() {
@@ -49,16 +98,14 @@ class _NewWidgetState extends State<NewWidget> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(title: const Text('Title'), centerTitle: true),
+          appBar: AppBar(title: Text(_htmlTitle, style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w600)), centerTitle: false),
           body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(
-                //height: 40,
-                //width: double.infinity,
-                child: Center(child: Text('cross')),
-              ),
+              Text(_cors, style: const TextStyle(fontSize: 16, color: Colors.redAccent),),
               const Divider(),
-              const Expanded(child: Center(child: Text('1'))),
+              Expanded(child: Center(child: Text(_htmlText))),
               const Divider(),
               Row(
                 children: [
@@ -79,7 +126,7 @@ class _NewWidgetState extends State<NewWidget> {
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () => loadHtmlPage(),
                     style: ButtonStyle(
                       minimumSize: const MaterialStatePropertyAll(Size(100,60)),
                       backgroundColor:
